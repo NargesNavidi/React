8f706fa45fae960ab854266b1725c17799dbcd82
Narges Navidi <nargesnavidi@yahoo.com>
2021-08-05 03:05:25 -0700

    Edited and replaced images

--- src/components/Brief.js
+++ src/components/Brief.js
@@ -1,38 +1,33 @@
-import React from 'react';
-import './Brief.css';
-import { PortfolioButton } from './PortfolioButton';
-
+import React from 'react'
+import './Brief.css'
+import { PortfolioButton } from './PortfolioButton'
 
 function Brief(props) {
-    return (
-        <div className='project__container'>
-            <div className='brief__wrapper'>
-                <div className='brief__content'>
-                    <h3>Project Brief</h3>
-                    <p>{props.projectBrief}</p>
-                    <h3>Skill Used</h3>
-                    <p>{props.skills}</p>
-                </div>
-            </div>
-            <div className='site__wrapper' >
-                <div className='site__content' >
-                    <h3>{props.name}</h3>
-                    <p>{props.companyInfo} </p>
-                        <PortfolioButton
-                        className='btns'
-                        buttonStyle='btn--primary'
-                        buttonSize='btn--large'
-                        href={props.href}
-                        >
-                            View Website
-                        </PortfolioButton>
-                </div>
-        
+  return (
+    <div className='project__container'>
+      <div className='brief__wrapper'>
+        <div className='brief__content'>
+          <h3>Project Brief</h3>
+          <p>{props.projectBrief}</p>
+          <h3>Skill Used</h3>
+          <p>{props.skills}</p>
         </div>
-
+      </div>
+      <div className='site__wrapper'>
+        <div className='site__content'>
+          <h3>{props.name}</h3>
+          <p>{props.companyInfo} </p>
+          <PortfolioButton
+            className='btns'
+            buttonStyle='btn--primary'
+            buttonSize='btn--large'
+            href={props.href}>
+            View Website
+          </PortfolioButton>
         </div>
-    )
+      </div>
+    </div>
+  )
 }
 
 export default Brief
-
